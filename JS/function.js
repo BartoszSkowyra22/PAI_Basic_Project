634f6cd7b46a8ee1987c5f528ba963d0d7510ca2
Bartosz S <skowyrab@gmail.com>
2023-03-22 00:04:10 +0100

    Lab6 Js Basic

---
+++ JS/function.js
@@ -0,0 +1,63 @@
+
+//Zadanie 6.2 - Problem ze wzorem - za duże wyniki
+function obliczRaty() {
+    //Pobranie danych
+    let kwotaPozyczki = parseInt(document.getElementById('kwotaObliczen').value);
+    let ileRat = parseInt(document.getElementById('liczbaRat').value);
+    let procentRocznie = document.getElementById('procent').value;
+
+    //Obliczenia
+    let procentMiesiecznie = procentRocznie / 12;
+    let rataWynik = (kwotaPozyczki * procentMiesiecznie) / (1 - (1 / (1 + procentMiesiecznie)**ileRat));
+
+    //Pobranie pól wynikowych
+    let rataMiesieczna = document.getElementById('rataMiesieczna');
+    let kwotaZOdsetkami = document.getElementById('kwotaZOdsetkami');
+
+    //Wyświetlenie wyniku
+    if (isNaN(rataWynik) || isFinite(rataWynik)) {
+        alert("Wprowadzono błędne dane!");
+        rataMiesieczna.value = 0;
+        kwotaZOdsetkami.value = 0;
+    } else {
+        rataMiesieczna.value = rataWynik;
+        kwotaZOdsetkami.value = rataWynik * ileRat;
+    }
+}
+
+//Zadanie 6.3
+function obliczWynik(){
+    var tab = document.getElementsByName("dzialanie");
+    var op;
+    for(let i=0;i<tab.length;i++){
+        if(tab[i].checked) op = tab[i].value;
+    }
+
+    let varX = parseFloat(document.getElementById('varX').value);
+    let varY = parseFloat(document.getElementById('varY').value);
+    let wynikKalkulatora = document.getElementById('wynikKalkulatora');
+    switch (op){
+        case '+':
+            wynikKalkulatora.value= varX + varY;
+            break;
+        case '-':
+            wynikKalkulatora.value = varX - varY;
+            break;
+        case '*':
+            wynikKalkulatora.value = varX * varY;
+            break;
+        case '/':
+            if (varY === 0) {
+                alert("Błędne dane!");
+                wynikKalkulatora.value = 0;
+                break;
+            } else {
+                wynikKalkulatora.value = varX / varY;
+                break;
+            }
+    }
+}
+
+
+
+
